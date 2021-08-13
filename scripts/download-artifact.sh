98064649f3a083f1906b0594727f09ba595c462e
Erik Marks <rekmarks@protonmail.com>
2021-08-12 22:38:02 -0700

    Use the GitHub Status API instead of pass/fail

--- scripts/download-artifact.sh
+++ scripts/download-artifact.sh
@@ -8,32 +8,7 @@ set -o pipefail
 # directory, from the successful workflow run corresponding to the specified
 # workflow name, pull request base branch, and GitHub repository identifier.
 
-# The path to the directory where the artifact files will be downloaded.
-ARTIFACTS_DIR_PATH=${1}
-
-if [[ -z $ARTIFACTS_DIR_PATH ]]; then
-  echo "Error: No artifacts directory specified."
-  exit 1
-fi
-
-# The name of the artifact to download.
-ARTIFACT_NAME=${2}
-
-if [[ -z $ARTIFACT_NAME ]]; then
-  echo "Error: No artifact name specified."
-  exit 1
-fi
-
-# Inputs 3-5 are used to identify the workflow run to download artifacts from.
-
-WORKFLOW_NAME=${3}
-
-if [[ -z $WORKFLOW_NAME ]]; then
-  echo "Error: No workflow name specified."
-  exit 1
-fi
-
-PULL_REQUEST_BASE_BRANCH=${4}
+PULL_REQUEST_BASE_BRANCH=${1}
 
 if [[ -z $PULL_REQUEST_BASE_BRANCH ]]; then
   echo "Error: No pull request base branch specified."
@@ -42,10 +17,35 @@ fi
 
 PULL_REQUEST_BASE_BRANCH="origin/${PULL_REQUEST_BASE_BRANCH}"
 
-GITHUB_REPOSITORY=${5}
+GITHUB_REPOSITORY=${2}
 
 if [[ -z $GITHUB_REPOSITORY ]]; then
   echo "Error: No GitHub repository identifier specified."
+  exit 1
+fi
+
+# The path to the directory where the artifact files will be downloaded.
+ARTIFACTS_DIR_PATH=${3}
+
+if [[ -z $ARTIFACTS_DIR_PATH ]]; then
+  echo "Error: No artifacts directory specified."
+  exit 1
+fi
+
+# The name of the artifact to download.
+ARTIFACT_NAME=${4}
+
+if [[ -z $ARTIFACT_NAME ]]; then
+  echo "Error: No artifact name specified."
+  exit 1
+fi
+
+# Inputs 3-5 are used to identify the workflow run to download artifacts from.
+
+WORKFLOW_NAME=${5}
+
+if [[ -z $WORKFLOW_NAME ]]; then
+  echo "Error: No workflow name specified."
   exit 1
 fi
 
---
+++ scripts/set-commit-status.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+set -o pipefail
+
+GITHUB_REPOSITORY=${1}
+
+if [[ -z $GITHUB_REPOSITORY ]]; then
+  echo "Error: No GitHub repository identifier specified."
+  exit 1
+fi
+
+IS_RELEASE=${2}
+
+if [[ -z $IS_RELEASE ]]; then
+  echo 'Error: No "is release" input specified.'
+  exit 1
+fi
+
+NUM_OTHER_APPROVING_REVIEWERS=${3}
+
+if [[ -z $NUM_OTHER_APPROVING_REVIEWERS ]]; then
+  echo "Error: No count of other approving reviewers specified."
+  exit 1
+fi
+
+HEAD_COMMIT_SHA=$(git show-ref -s HEAD)
+
+if [[ -z $HEAD_COMMIT_SHA ]]; then
+  echo "Error: \"git show-ref -s HEAD\" returned an empty value."
+  exit 1
+fi
+
+# Finally, compute the status for the current commit and set it via the GitHub API.
+
+COMMIT_STATUS_DESCRIPTION="Whether this PR meets the additional reviewer requirement for releases."
+COMMIT_STATUS="pending" # the default for releases
+
+if [[ "$IS_RELEASE" == "false" ]]; then
+  echo "The PR is not a release PR. Setting status to success by default."
+  COMMIT_STATUS="success"
+elif (( NUM_OTHER_APPROVING_REVIEWERS > 0 )); then
+  echo "Success! Found approving reviews from organization members."
+  COMMIT_STATUS="success"
+else
+  echo "Failure: Did not find approving reviews from other organization members."
+fi
+
+# https://cli.github.com/manual/gh_api
+# https://docs.github.com/en/rest/reference/repos#create-a-commit-status
+
+gh api "https://api.github.com/repos/${GITHUB_REPOSITORY}/statuses/${HEAD_COMMIT_SHA}" \
+  -X "POST" \
+  -H "Accept: application/vnd.github.v3+json" \
+  -d '{
+    "context": "MetaMask/action-require-additional-reviewer",
+    "description": "'"$COMMIT_STATUS_DESCRIPTION"'",
+    "state": "'"$COMMIT_STATUS"'",
+    "target_url": "https://github.com/MetaMask/action-require-additional-reviewer"
+  }'
+
+# The action should never fail, only set a status for the release branch HEAD
+# commit.
+exit 0
