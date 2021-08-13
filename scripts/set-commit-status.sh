93c1725cc8d6ad2079c3e3936c5ef799b982d579
Erik Marks <rekmarks@protonmail.com>
2021-08-13 10:12:37 -0700

    fixup! Use git log to get the HEAD commit

--- scripts/set-commit-status.sh
+++ scripts/set-commit-status.sh
@@ -38,7 +38,7 @@ if [[ $IS_RELEASE == "true" && -z $NUM_OTHER_APPROVING_REVIEWERS ]]; then
 fi
 
 # git rev-parse doesn't always work, for unknown reasons
-HEAD_COMMIT_SHA=$(git log --format=format:%H | grep -m 1 -o '^\w\+\')
+HEAD_COMMIT_SHA=$(git log --format=format:%H | grep -m 1 -o '^\w\+')
 
 if [[ -z $HEAD_COMMIT_SHA ]]; then
   echo 'Error: Failed to get HEAD commit sha.'
